e279900ec20c120676680763b9f9c47a8c8bc2a1
dongzhang <wb-zd929541@alibaba-inc.com>
2021-10-26 16:01:35 +0800

    修改服务器地址

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
 
 		// Paths
 		assetsSubDirectory: 'static',
-		assetsPublicPath: './',
+		assetsPublicPath: '/',
 		// 代理列表, 是否开启代理通过[./dev.env.js]配置
 		proxyTable: devEnv.OPEN_PROXY === false ? {} : {
 			'/admin': {
